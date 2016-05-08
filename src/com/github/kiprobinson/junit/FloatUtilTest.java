62fd26c38045a0a1cd67c4b4aeb03ad1abdcef61
kiprobinson <kip.robinson@gmail.com>
2016-05-07 23:03:59 -0400

    Adding FloatUtil, and adding public constants to DoubleUtil

---
+++ src/com/github/kiprobinson/junit/FloatUtilTest.java
@@ -0,0 +1,144 @@
+package com.github.kiprobinson.junit;
+
+import static org.junit.Assert.*;
+
+import com.github.kiprobinson.util.FloatUtil;
+
+import org.junit.Test;
+
+
+/**
+ * JUnit tests for BigFraction class.
+ * 
+ * @author Kip Robinson, <a href="https://github.com/kiprobinson">https://github.com/kiprobinson</a>
+ */
+public class FloatUtilTest {
+  
+  
+  @Test
+  public void testValues() {
+    runTest( 1.0f, 0, 0x7f, 0, 0x000000, false);
+    runTest(-1.0f, 1, 0x7f, 0, 0x000000, false);
+    
+    runTest( 2.0f, 0, 0x80, 1, 0x000000, false);
+    runTest(-2.0f, 1, 0x80, 1, 0x000000, false);
+    
+    runTest( 8.0f, 0, 0x82, 3, 0x000000, false);
+    runTest(-8.0f, 1, 0x82, 3, 0x000000, false);
+    
+    runTest( 0.5f, 0, 0x7e, -1, 0x000000, false);
+    runTest(-0.5f, 1, 0x7e, -1, 0x000000, false);
+    
+    runTest( 0.125f, 0, 0x7c, -3, 0x000000, false);
+    runTest(-0.125f, 1, 0x7c, -3, 0x000000, false);
+    
+    runTest( 1.625f, 0, 0x7f, 0, 0x500000, false);
+    runTest(-1.625f, 1, 0x7f, 0, 0x500000, false);
+    
+    runTest( 0.0f, 0, 0x000, -127, 0x000000, false);
+    runTest(-0.0f, 1, 0x000, -127, 0x000000, false);
+    
+    runTest( Float.MAX_VALUE, 0, 0xfe, 127, 0x7fffff, false);
+    runTest(-Float.MAX_VALUE, 1, 0xfe, 127, 0x7fffff, false);
+    
+    runTest( Float.MIN_NORMAL, 0, 0x001, -126, 0x000000, false);
+    runTest(-Float.MIN_NORMAL, 1, 0x001, -126, 0x000000, false);
+    
+    //smallest subnormals
+    runTest( Float.MIN_VALUE, 0, 0x000, -127, 0x000001, true);
+    runTest(-Float.MIN_VALUE, 1, 0x000, -127, 0x000001, true);
+    
+    //largest subnormals
+    runTest(Float.intBitsToFloat(0x007fffff), 0, 0x000, -127, 0x7fffff, true);
+    runTest(Float.intBitsToFloat(0x807fffff), 1, 0x000, -127, 0x7fffff, true);
+    
+    runTest( Float.POSITIVE_INFINITY, 0, 0xff, 128, 0x000000, false);
+    runTest(-Float.POSITIVE_INFINITY, 1, 0xff, 128, 0x000000, false);
+    
+    runTest(-Float.NEGATIVE_INFINITY, 0, 0xff, 128, 0x000000, false);
+    runTest( Float.NEGATIVE_INFINITY, 1, 0xff, 128, 0x000000, false);
+    
+    //test NaN values. You can't negate a NaN, so the only way to test it is to construct it directly
+    runTest(Float.NaN, 0, 0xff, 128, 0x400000, false);
+    runTest(Float.intBitsToFloat(0x7fc00000), 0, 0xff, 128, 0x400000, false);
+    runTest(Float.intBitsToFloat(0xffc00000), 1, 0xff, 128, 0x400000, false);
+    runTest(Float.intBitsToFloat(0x7f800001), 0, 0xff, 128, 0x000001, false);
+    runTest(Float.intBitsToFloat(0xff800001), 1, 0xff, 128, 0x000001, false);
+    runTest(Float.intBitsToFloat(0x7fffffff), 0, 0xff, 128, 0x7fffff, false);
+    runTest(Float.intBitsToFloat(0xffffffff), 1, 0xff, 128, 0x7fffff, false);
+    
+    runTest(Float.intBitsToFloat(0xdeadbeef), 1, 0xbd, 0x3e, 0x2dbeef, false);
+    runTest(Float.intBitsToFloat(0xcafebabe), 1, 0x95, 0x16, 0x7ebabe, false);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testSmallSign() {
+    FloatUtil.getFloat(-1, 0, 0);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testLargeSign() {
+    FloatUtil.getFloat(2, 0, 0);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testSmallExponent_Bits() {
+    FloatUtil.getFloat(0, -1, 0, true);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testLargeExponent_Bits() {
+    FloatUtil.getFloat(0, 0x800, 0, true);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testSmallExponent_Signed() {
+    FloatUtil.getFloat(0, -1024, 0, false);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testLargeExponent_Signed() {
+    FloatUtil.getFloat(0, 0x401, 0, false);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testSmallMantissa() {
+    FloatUtil.getFloat(0, 0, -1);
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void testLargeMantissa() {
+    FloatUtil.getFloat(0, 0, 0x800000);
+  }
+  
+  
+  /**
+   * Takes a float value and the expected components, and makes sure that all functions return the expected value.
+   */
+  private void runTest(float val, int sign, int exponentBits, int exponent, int mantissa, boolean isSubnormal) {
+    assertEquals("getSign(" + val + ")", sign, FloatUtil.getSign(val));
+    assertEquals("getExponentBits(" + val + ")", exponentBits, FloatUtil.getExponentBits(val));
+    assertEquals("getExponent(" + val + ")", exponent, FloatUtil.getExponent(val));
+    assertEquals("getMantissa(" + val + ")", mantissa, FloatUtil.getMantissa(val));
+    assertEquals("isSubnormal(" + val + ")", isSubnormal, FloatUtil.isSubnormal(val));
+    
+    float tmp1 = FloatUtil.getFloat(sign, exponent, mantissa, false);
+    assertEquals("getFloat(" + val + ") - with signed exponent", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp1));
+    
+    float tmp2 = FloatUtil.getFloat(sign, exponentBits, mantissa, true);
+    assertEquals("getFloat(" + val + ") - with exponent as bits", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp2));
+    
+    int[] parts = FloatUtil.getAllParts(val, false);
+    assertEquals("getAllParts(" + val + ") - signed exponent - sign", sign, parts[0]);
+    assertEquals("getAllParts(" + val + ") - signed exponent - exponent", exponent, parts[1]);
+    assertEquals("getAllParts(" + val + ") - signed exponent - mantissa", mantissa, parts[2]);
+    assertEquals("getAllParts(" + val + ") - signed exponent - isSubnormal - " + parts[3], isSubnormal, (parts[3] != 0L));
+    
+    parts = FloatUtil.getAllParts(val, true);
+    assertEquals("getAllParts(" + val + ") - unsigned exponent - sign", sign, parts[0]);
+    assertEquals("getAllParts(" + val + ") - unsigned exponent - exponent", exponentBits, parts[1]);
+    assertEquals("getAllParts(" + val + ") - unsigned exponent - mantissa", mantissa, parts[2]);
+    assertEquals("getAllParts(" + val + ") - unsigned exponent - isSubnormal", isSubnormal, (parts[3] != 0L));
+    
+  }
+}
---
+++ src/com/github/kiprobinson/util/FloatUtil.java
@@ -0,0 +1,209 @@
+package com.github.kiprobinson.util;
+
+/**
+ * Additional utilities for working with floay values. Useful to prevent hard-coding IEEE 754 constants in code.
+ * Consider this a complement to the methods provided in {@link Float}.
+ * 
+ * @author Kip Robinson, <a href="https://github.com/kiprobinson">https://github.com/kiprobinson</a>
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/Float-precision_floating-point_format">Wikipedia overview of IEEE 754 float-precision floating point specifications</a>
+ */
+public final class FloatUtil
+{
+  /** Mask to get sign bit in a raw float value. */
+  private final static int SIGN_MASK = 0x80000000;
+  /** Bit position of the sign bit in IEEE 754 float-precision binary representation. */
+  private final static int SIGN_POS = 31;
+  
+  /** Mask to get exponent bits in a raw float value. */
+  private final static int EXPONENT_BITS_MASK = 0x7f800000;
+  /** Bit position of the exponent bits in IEEE 754 float-precision binary representation. */
+  private final static int EXPONENT_POS = 23;
+  /** Exponent offset, per IEEE 754 spec. */
+  private final static int EXPONENT_OFFSET = 0x7f;
+  
+  /** Mask to get mantissa bits in a raw float value. */
+  private final static int MANTISSA_MASK = 0x7fffff;
+  
+  /** The largest permitted mantissa value. */
+  public final static int MAX_MANTISSA = MANTISSA_MASK;
+  
+  /** The largest permitted exponent value, when using raw exponent bits. */
+  public final static int MAX_EXPONENT_BITS = (EXPONENT_BITS_MASK >> EXPONENT_POS);
+  
+  /** The smallest permitted exponent value, when using an exponent with offset. */
+  public final static int MIN_EXPONENT = -EXPONENT_OFFSET;
+  /** The largest permitted exponent value, when using an exponent with offset. */
+  public final static int MAX_EXPONENT = MAX_EXPONENT_BITS - EXPONENT_OFFSET;
+  
+  
+  /** Hide default constructor to prevent instantiation. */
+  private FloatUtil() {}
+  
+  /**
+   * Returns the sign bit (bit 63). 0=positive, 1=negative. This is returned even for NaN values.
+   * 
+   * @param d a float value
+   * @return the sign bit
+   */
+  public static int getSign(float d)
+  {
+    return (int)((Float.floatToRawIntBits(d) & SIGN_MASK) >>> SIGN_POS);
+  }
+  
+  /**
+   * Returns the exponent, after adding the exponent offset to the exponent bits. In other words, the value
+   * in bits 62-52, plus 0x3ff.<br>
+   * <br>
+   * Examples:<ul>
+   *   <li>{@code getExponent(0.5) == -1} because {@code 1.0 == 1.0 * 2^-1}</li>
+   *   <li>{@code getExponent(1.0) ==  0} because {@code 1.0 == 1.0 * 2^0}</li>
+   *   <li>{@code getExponent(2.0) ==  1} because {@code 2.0 == 1.0 * 2^1}</li>
+   *   <li>Subnormal values: Always returns -1023.</li>
+   *   <li>Zero: Always returns -1023.</li>
+   *   <li>Infinity: Always returns 1024.</li>
+   *   <li>NaN: Always returns 1024.</li>
+   *   </ul>
+   * 
+   * @param d any float value
+   * @return adjusted exponent
+   */
+  public static int getExponent(float d)
+  {
+    return getExponentBits(d) - EXPONENT_OFFSET;
+  }
+  
+  /**
+   * Returns the raw exponent bits, without adjusting for the offset. In other words, the value
+   * in bits 62-52.<br>
+   * <br>
+   * Examples:<ul>
+   *   <li>{@code getExponentBits(0.5) == 0x3fe}</li>
+   *   <li>{@code getExponentBits(1.0) == 0x3ff}</li>
+   *   <li>{@code getExponentBits(2.0) == 0x400}</li>
+   *   <li>Subnormal values: Always returns 0x000.</li>
+   *   <li>Zero: Always returns 0x000.</li>
+   *   <li>Infinity: Always returns 0x7ff.</li>
+   *   <li>NaN: Always returns 0x7ff.</li>
+   * </ul>
+   * 
+   * @param d a float value
+   * @return raw exponent bits
+   */
+  public static int getExponentBits(float d)
+  {
+    return (int)((Float.floatToRawIntBits(d) & EXPONENT_BITS_MASK) >>> EXPONENT_POS);
+  }
+  
+  /**
+   * Returns the mantissa bits (bits 51-0). Returned even for NaN values.
+   * @param d a float value
+   * @return mantissa bits
+   */
+  public static int getMantissa(float d)
+  {
+    return Float.floatToRawIntBits(d) & MANTISSA_MASK;
+  }
+  
+  /**
+   * Returns whether or not this float is a subnormal value.
+   * @param d a float value
+   * @return whether or not this float is a subnormal value
+   */
+  public static boolean isSubnormal(float d)
+  {
+    int bits = Float.floatToRawIntBits(d);
+    return ((bits & EXPONENT_BITS_MASK) == 0) && ((bits & MANTISSA_MASK) != 0);
+  }
+  
+  
+  /**
+   * Returns an array containing the parts of the float. Avoids the overhead of four separate function calls.
+   * 
+   * @param d a float value
+   * @return array with four elements:<ul>
+   *     <li>return[0]: Equivalent to getSign(d)</li>
+   *     <li>return[1]: Equivalent to getExponent(d)</li>
+   *     <li>return[2]: Equivalent to getMantissa(d)</li>
+   *     <li>return[3]: Equivalent to isSubnormal(d) - Uses zero for false, non-zero for true.</li>
+   * </ul>
+   */
+  public static int[] getAllParts(float d)
+  {
+    return getAllParts(d, false);
+  }
+  
+  /**
+   * Returns an array containing the parts of the float. Avoids the overhead of four separate function calls.
+   * 
+   * @param d a float value
+   * @param exponentAsBits whether to return exponent as raw bits rather than adjusted value
+   * @return array with four elements:<ul>
+   *     <li>return[0]: Equivalent to getSign(d)</li>
+   *     <li>return[1]: Equivalent to (exponentAsBits ? getExponentBits(d) : getExponent(d))</li>
+   *     <li>return[2]: Equivalent to getMantissa(d)</li>
+   *     <li>return[3]: Equivalent to isSubnormal(d) - Uses zero for false, non-zero for true.</li>
+   * </ul>
+   */
+  public static int[] getAllParts(float d, boolean exponentAsBits)
+  {
+    int[] segments = new int[4];
+    int bits = Float.floatToRawIntBits(d);
+    
+    segments[0] = (bits & SIGN_MASK) >>> SIGN_POS;
+    segments[1] = (bits & EXPONENT_BITS_MASK) >>> EXPONENT_POS;
+    segments[2] = bits & MANTISSA_MASK;
+    segments[3] = (segments[1] == 0 && segments[2] != 0 ? 1 : 0);
+    
+    if(!exponentAsBits)
+      segments[1] -= EXPONENT_OFFSET;
+    
+    return segments;
+  }
+  
+  /**
+   * Creates a new float primitive using the provided component bits. Assumes the exponent parameter is signed.
+   * 
+   * @param sign sign bit
+   * @param exponent adjusted exponent
+   * @param mantissa mantissa bits
+   * 
+   * @return The float value represented by the provided binary parts.
+   * 
+   * @throws IllegalArgumentException if any of the parts contain invalid bits.
+   */
+  public static float getFloat(int sign, int exponent, int mantissa)
+  {
+    return getFloat(sign, exponent, mantissa, false);
+  }
+  
+  /**
+   * Creates a new float primitive using the provided component bits.
+   * 
+   * @param sign            sign bit
+   * @param exponent        exponent (either raw bits or adjusted value)
+   * @param mantissa        mantissa bits
+   * @param exponentAsBits  If true, assumes that exponent parameter represents the actual exponent bits. If false,
+   *                        IEEE exponent offset value will be added to the offset to get the bits.
+   * 
+   * @return The float value represented by the provided binary parts.
+   * 
+   * @throws IllegalArgumentException if any of the parts contain invalid bits.
+   */
+  public static float getFloat(int sign, int exponent, int mantissa, boolean exponentAsBits)
+  {
+    if(sign < 0 || sign > 1)
+      throw new IllegalArgumentException("Illegal sign bit: " + sign);
+    
+    if(0 != (mantissa & ~MANTISSA_MASK))
+      throw new IllegalArgumentException("Illegal mantissa: " + mantissa);
+    
+    int offsetExponent = (exponentAsBits ? exponent : exponent + EXPONENT_OFFSET);
+    
+    if(0 != (offsetExponent & ~MAX_EXPONENT_BITS))
+      throw new IllegalArgumentException("Illegal exponent: " + exponent);
+    
+    return Float.intBitsToFloat((((int)sign) << SIGN_POS) | (((int)offsetExponent) << EXPONENT_POS) | mantissa);
+  }
+
+}
