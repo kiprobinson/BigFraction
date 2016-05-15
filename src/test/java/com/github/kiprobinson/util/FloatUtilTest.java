5f230568ee8cd847917d90061f358d9371b34354
kiprobinson <kip.robinson@gmail.com>
2016-05-14 22:04:42 -0400

    Code Coverage - DoubleUtil and FloatUtil

--- src/test/java/com/github/kiprobinson/util/FloatUtilTest.java
+++ src/test/java/com/github/kiprobinson/util/FloatUtilTest.java
@@ -17,58 +17,58 @@ public class FloatUtilTest {
   
   @Test
   public void testValues() {
-    runTest( 1.0f, 0, 0x7f, 0, 0x000000, false);
-    runTest(-1.0f, 1, 0x7f, 0, 0x000000, false);
+    runTest( 1.0f, 0, 0x7f, 0, 0x000000, false, true);
+    runTest(-1.0f, 1, 0x7f, 0, 0x000000, false, true);
     
-    runTest( 2.0f, 0, 0x80, 1, 0x000000, false);
-    runTest(-2.0f, 1, 0x80, 1, 0x000000, false);
+    runTest( 2.0f, 0, 0x80, 1, 0x000000, false, true);
+    runTest(-2.0f, 1, 0x80, 1, 0x000000, false, true);
     
-    runTest( 8.0f, 0, 0x82, 3, 0x000000, false);
-    runTest(-8.0f, 1, 0x82, 3, 0x000000, false);
+    runTest( 8.0f, 0, 0x82, 3, 0x000000, false, true);
+    runTest(-8.0f, 1, 0x82, 3, 0x000000, false, true);
     
-    runTest( 0.5f, 0, 0x7e, -1, 0x000000, false);
-    runTest(-0.5f, 1, 0x7e, -1, 0x000000, false);
+    runTest( 0.5f, 0, 0x7e, -1, 0x000000, false, true);
+    runTest(-0.5f, 1, 0x7e, -1, 0x000000, false, true);
     
-    runTest( 0.125f, 0, 0x7c, -3, 0x000000, false);
-    runTest(-0.125f, 1, 0x7c, -3, 0x000000, false);
+    runTest( 0.125f, 0, 0x7c, -3, 0x000000, false, true);
+    runTest(-0.125f, 1, 0x7c, -3, 0x000000, false, true);
     
-    runTest( 1.625f, 0, 0x7f, 0, 0x500000, false);
-    runTest(-1.625f, 1, 0x7f, 0, 0x500000, false);
+    runTest( 1.625f, 0, 0x7f, 0, 0x500000, false, true);
+    runTest(-1.625f, 1, 0x7f, 0, 0x500000, false, true);
     
-    runTest( 0.0f, 0, 0x000, -127, 0x000000, false);
-    runTest(-0.0f, 1, 0x000, -127, 0x000000, false);
+    runTest( 0.0f, 0, 0x000, -127, 0x000000, false, true);
+    runTest(-0.0f, 1, 0x000, -127, 0x000000, false, true);
     
-    runTest( Float.MAX_VALUE, 0, 0xfe, 127, 0x7fffff, false);
-    runTest(-Float.MAX_VALUE, 1, 0xfe, 127, 0x7fffff, false);
+    runTest( Float.MAX_VALUE, 0, 0xfe, 127, 0x7fffff, false, true);
+    runTest(-Float.MAX_VALUE, 1, 0xfe, 127, 0x7fffff, false, true);
     
-    runTest( Float.MIN_NORMAL, 0, 0x001, -126, 0x000000, false);
-    runTest(-Float.MIN_NORMAL, 1, 0x001, -126, 0x000000, false);
+    runTest( Float.MIN_NORMAL, 0, 0x001, -126, 0x000000, false, true);
+    runTest(-Float.MIN_NORMAL, 1, 0x001, -126, 0x000000, false, true);
     
     //smallest subnormals
-    runTest( Float.MIN_VALUE, 0, 0x000, -127, 0x000001, true);
-    runTest(-Float.MIN_VALUE, 1, 0x000, -127, 0x000001, true);
+    runTest( Float.MIN_VALUE, 0, 0x000, -127, 0x000001, true, true);
+    runTest(-Float.MIN_VALUE, 1, 0x000, -127, 0x000001, true, true);
     
     //largest subnormals
-    runTest(Float.intBitsToFloat(0x007fffff), 0, 0x000, -127, 0x7fffff, true);
-    runTest(Float.intBitsToFloat(0x807fffff), 1, 0x000, -127, 0x7fffff, true);
+    runTest(Float.intBitsToFloat(0x007fffff), 0, 0x000, -127, 0x7fffff, true, true);
+    runTest(Float.intBitsToFloat(0x807fffff), 1, 0x000, -127, 0x7fffff, true, true);
     
-    runTest( Float.POSITIVE_INFINITY, 0, 0xff, 128, 0x000000, false);
-    runTest(-Float.POSITIVE_INFINITY, 1, 0xff, 128, 0x000000, false);
+    runTest( Float.POSITIVE_INFINITY, 0, 0xff, 128, 0x000000, false, false);
+    runTest(-Float.POSITIVE_INFINITY, 1, 0xff, 128, 0x000000, false, false);
     
-    runTest(-Float.NEGATIVE_INFINITY, 0, 0xff, 128, 0x000000, false);
-    runTest( Float.NEGATIVE_INFINITY, 1, 0xff, 128, 0x000000, false);
+    runTest(-Float.NEGATIVE_INFINITY, 0, 0xff, 128, 0x000000, false, false);
+    runTest( Float.NEGATIVE_INFINITY, 1, 0xff, 128, 0x000000, false, false);
     
     //test NaN values. You can't negate a NaN, so the only way to test it is to construct it directly
-    runTest(Float.NaN, 0, 0xff, 128, 0x400000, false);
-    runTest(Float.intBitsToFloat(0x7fc00000), 0, 0xff, 128, 0x400000, false);
-    runTest(Float.intBitsToFloat(0xffc00000), 1, 0xff, 128, 0x400000, false);
-    runTest(Float.intBitsToFloat(0x7f800001), 0, 0xff, 128, 0x000001, false);
-    runTest(Float.intBitsToFloat(0xff800001), 1, 0xff, 128, 0x000001, false);
-    runTest(Float.intBitsToFloat(0x7fffffff), 0, 0xff, 128, 0x7fffff, false);
-    runTest(Float.intBitsToFloat(0xffffffff), 1, 0xff, 128, 0x7fffff, false);
+    runTest(Float.NaN, 0, 0xff, 128, 0x400000, false, false);
+    runTest(Float.intBitsToFloat(0x7fc00000), 0, 0xff, 128, 0x400000, false, false);
+    runTest(Float.intBitsToFloat(0xffc00000), 1, 0xff, 128, 0x400000, false, false);
+    runTest(Float.intBitsToFloat(0x7f800001), 0, 0xff, 128, 0x000001, false, false);
+    runTest(Float.intBitsToFloat(0xff800001), 1, 0xff, 128, 0x000001, false, false);
+    runTest(Float.intBitsToFloat(0x7fffffff), 0, 0xff, 128, 0x7fffff, false, false);
+    runTest(Float.intBitsToFloat(0xffffffff), 1, 0xff, 128, 0x7fffff, false, false);
     
-    runTest(Float.intBitsToFloat(0xdeadbeef), 1, 0xbd, 0x3e, 0x2dbeef, false);
-    runTest(Float.intBitsToFloat(0xcafebabe), 1, 0x95, 0x16, 0x7ebabe, false);
+    runTest(Float.intBitsToFloat(0xdeadbeef), 1, 0xbd, 0x3e, 0x2dbeef, false, true);
+    runTest(Float.intBitsToFloat(0xcafebabe), 1, 0x95, 0x16, 0x7ebabe, false, true);
   }
   
   @Test(expected=IllegalArgumentException.class)
@@ -115,30 +115,41 @@ public class FloatUtilTest {
   /**
    * Takes a float value and the expected components, and makes sure that all functions return the expected value.
    */
-  private void runTest(float val, int sign, int exponentBits, int exponent, int mantissa, boolean isSubnormal) {
+  @SuppressWarnings("deprecation")
+  private void runTest(float val, int sign, int exponentBits, int exponent, int mantissa, boolean isSubnormal, boolean isFinite) {
     assertEquals("getSign(" + val + ")", sign, FloatUtil.getSign(val));
     assertEquals("getExponentBits(" + val + ")", exponentBits, FloatUtil.getExponentBits(val));
     assertEquals("getExponent(" + val + ")", exponent, FloatUtil.getExponent(val));
     assertEquals("getMantissa(" + val + ")", mantissa, FloatUtil.getMantissa(val));
     assertEquals("isSubnormal(" + val + ")", isSubnormal, FloatUtil.isSubnormal(val));
+    assertEquals("isFinite(" + val + ")", isFinite, FloatUtil.isFinite(val));
     
-    float tmp1 = FloatUtil.getFloat(sign, exponent, mantissa, false);
-    assertEquals("getFloat(" + val + ") - with signed exponent", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp1));
+    float tmp = FloatUtil.getFloat(sign, exponent, mantissa);
+    assertEquals("getFloat(" + val + ") - with signed exponent", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp));
     
-    float tmp2 = FloatUtil.getFloat(sign, exponentBits, mantissa, true);
-    assertEquals("getFloat(" + val + ") - with exponent as bits", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp2));
+    tmp = FloatUtil.getFloat(sign, exponent, mantissa, false);
+    assertEquals("getFloat(" + val + ", false) - with signed exponent", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp));
     
-    int[] parts = FloatUtil.getAllParts(val, false);
+    tmp = FloatUtil.getFloat(sign, exponentBits, mantissa, true);
+    assertEquals("getFloat(" + val + ", true) - with exponent as bits", Float.floatToRawIntBits(val), Float.floatToRawIntBits(tmp));
+    
+    int[] parts = FloatUtil.getAllParts(val);
     assertEquals("getAllParts(" + val + ") - signed exponent - sign", sign, parts[0]);
     assertEquals("getAllParts(" + val + ") - signed exponent - exponent", exponent, parts[1]);
     assertEquals("getAllParts(" + val + ") - signed exponent - mantissa", mantissa, parts[2]);
     assertEquals("getAllParts(" + val + ") - signed exponent - isSubnormal - " + parts[3], isSubnormal, (parts[3] != 0L));
     
+    parts = FloatUtil.getAllParts(val, false);
+    assertEquals("getAllParts(" + val + ", false) - signed exponent - sign", sign, parts[0]);
+    assertEquals("getAllParts(" + val + ", false) - signed exponent - exponent", exponent, parts[1]);
+    assertEquals("getAllParts(" + val + ", false) - signed exponent - mantissa", mantissa, parts[2]);
+    assertEquals("getAllParts(" + val + ", false) - signed exponent - isSubnormal - " + parts[3], isSubnormal, (parts[3] != 0L));
+    
     parts = FloatUtil.getAllParts(val, true);
-    assertEquals("getAllParts(" + val + ") - unsigned exponent - sign", sign, parts[0]);
-    assertEquals("getAllParts(" + val + ") - unsigned exponent - exponent", exponentBits, parts[1]);
-    assertEquals("getAllParts(" + val + ") - unsigned exponent - mantissa", mantissa, parts[2]);
-    assertEquals("getAllParts(" + val + ") - unsigned exponent - isSubnormal", isSubnormal, (parts[3] != 0L));
+    assertEquals("getAllParts(" + val + ", true) - unsigned exponent - sign", sign, parts[0]);
+    assertEquals("getAllParts(" + val + ", true) - unsigned exponent - exponent", exponentBits, parts[1]);
+    assertEquals("getAllParts(" + val + ", true) - unsigned exponent - mantissa", mantissa, parts[2]);
+    assertEquals("getAllParts(" + val + ", true) - unsigned exponent - isSubnormal", isSubnormal, (parts[3] != 0L));
     
   }
 }
